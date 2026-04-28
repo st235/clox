37453cfcbaf1649d0c14222d5f0538c67360774d
Alex Dadukin <alexanderdadukin@gmail.com>
2026-04-28 21:44:16 +0100

    Add opcodes for return and constant

---
+++ include/chunk.h
@@ -0,0 +1,26 @@
+#ifndef CLOX_CHUNK_H_
+#define CLOX_CHUNK_H_
+
+#include "common.h"
+#include "value.h"
+
+typedef enum {
+    OP_CONSTANT,
+    OP_RETURN,
+} OpCode;
+
+typedef struct {
+    int count;
+    int capacity;
+    uint8_t* code;
+    ValueArray constants;
+    int* lines;
+} Chunk;
+
+void initChunk(Chunk* chunk);
+void writeChunk(Chunk* chunk, uint8_t byte, int line);
+void freeChunk(Chunk* chunk);
+
+int addConstant(Chunk* chunk, Value value);
+
+#endif  // CLOX_CHUNK_H_
